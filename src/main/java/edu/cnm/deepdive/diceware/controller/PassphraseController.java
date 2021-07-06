85500cc4b52a714f6b8ef68e11c7d69d74f9f962
Marshall Eldridge <l2marshall@gmail.com>
2021-07-06 15:21:00 -0600

    implemented html response

--- src/main/java/edu/cnm/deepdive/diceware/controller/PassphraseController.java
+++ src/main/java/edu/cnm/deepdive/diceware/controller/PassphraseController.java
@@ -2,12 +2,16 @@ package edu.cnm.deepdive.diceware.controller;
 
 import edu.cnm.deepdive.diceware.service.PassphraseGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/passphrases")
 public class PassphraseController {
 
@@ -18,8 +22,15 @@ public class PassphraseController {
     this.generator = generator;
   }
 
-  @GetMapping("/transient")
+  @GetMapping(value = "/transient", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody
   public String[] generate(@RequestParam(defaultValue = "5") int length) {
     return generator.generate(length);
+  }
+
+  @GetMapping(value = "/transient", produces = MediaType.TEXT_HTML_VALUE)
+  public String generateHtml(@RequestParam(defaultValue = "5") int length, Model model) {
+    model.addAttribute("words", generator.generate(length));
+    return "passphrase";
   }
 }
